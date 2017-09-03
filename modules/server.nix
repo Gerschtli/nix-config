db7c777c570eae89bac759273482a0c947641a81
Tobias Happ <tobias.happ@gmx.de>
2017-09-03 23:40:16 +0200

    Configures goldenRiverJazztett and autoGoldenRiverJazztett

--- modules/server.nix
+++ modules/server.nix
@@ -1,5 +1,66 @@
 { config, pkgs, ... }:
 
+let
+
+  fetchBitBucket = pkgs.callPackage
+    (
+      builtins.scopedImport {
+        __nixPath = [
+          {
+            path = pkgs.writeText "ssh_config" ''
+              Host bitbucket.org
+                IdentityFile /etc/nixos/misc/id_rsa.bitbucket-deploy
+                StrictHostKeyChecking no
+                UserKnownHostsFile=/dev/null
+            '';
+            prefix="ssh-config-file";
+          }
+        ] ++ __nixPath;
+      }
+      <nixpkgs/pkgs/build-support/fetchgit/private.nix>
+    ) { };
+
+  autoGoldenRiverJazztett = pkgs.stdenv.mkDerivation rec {
+    name = "auto-golden-river-jazztett-${version}";
+    version = "2017-09-03";
+
+    src = fetchBitBucket {
+      url = "git@bitbucket.org:tobiashapp/auto-golden-river-jazztett.git";
+      rev = "82025349769f8c011791c212168eddd822d942c8";
+      sha256 = "0kvy60s1p3kxxk43i6pd4gka64plqd46dh2dfs24wmz57rhzh10s";
+    };
+
+    phases = [ "unpackPhase" "installPhase" "fixupPhase" ];
+
+    installPhase = "cp -R . \$out";
+
+    postFixup = ''
+      rm $out/dump.sql $out/install.sh
+    '';
+  };
+
+  goldenRiverJazztett = pkgs.stdenv.mkDerivation rec {
+    name = "golden-river-jazztett-${version}";
+    version = "2017-09-03";
+
+    src = fetchBitBucket {
+      url = "git@bitbucket.org:tobiashapp/golden-river-jazztett.git";
+      rev = "eac760aafff855f1bbb5430c54cec5b890df5d04";
+      sha256 = "147v84r5p1hvdj9qx54q3p6xynf8hkic4lhh245rilvsmkawj28f";
+    };
+
+    phases = [ "unpackPhase" "installPhase" "fixupPhase" ];
+
+    installPhase = "cp -R . \$out";
+
+    postFixup = ''
+      find $out -maxdepth 1 -type f -exec rm {} \+
+      find $out -type f -name '*.sass' -o -name '*.scss' -exec rm {} \+
+    '';
+  };
+
+in
+
 {
   networking.firewall = {
     enable = true;
@@ -24,6 +85,31 @@
       logPerVirtualHost = true;
       enablePHP = true;
       adminAddr = "tobias.happ@gmx.de";
+      virtualHosts = [
+        {
+          hostName = "auto.goldenriverjazztett.de";
+          documentRoot = "${autoGoldenRiverJazztett}";
+          extraConfig = ''
+            <Directory ${autoGoldenRiverJazztett}>
+              Options -Indexes
+              DirectoryIndex index.php
+              AllowOverride All
+            </Directory>
+          '';
+        }
+        {
+          hostName = "goldenriverjazztett.de";
+          serverAliases = [ "www.goldenriverjazztett.de" ];
+          documentRoot = "${goldenRiverJazztett}/public";
+          extraConfig = ''
+            <Directory ${goldenRiverJazztett}/public>
+              Options -Indexes
+              DirectoryIndex index.php
+              AllowOverride All
+            </Directory>
+          '';
+        }
+      ];
     };
 
     mysql = {
