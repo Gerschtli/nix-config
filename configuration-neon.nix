41ab1052f580527aee30a49743cd2925cd6aaf7f
Tobias Happ <tobias.happ@gmx.de>
2017-09-04 10:58:37 +0200

    Adds configuration-neon.nix

---
+++ configuration-neon.nix
@@ -0,0 +1,24 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+
+    ./modules/general.nix
+
+    ./services/firewall.nix
+    ./services/ssh.nix
+
+    ./applications/goldenRiverJazztett.nix
+    ./applications/autoGoldenRiverJazztett.nix
+    ./applications/teamspeak.nix
+  ];
+
+  boot.loader.grub = {
+    device = "/dev/sda";
+    enable = true;
+    version = 2;
+  };
+
+  networking.hostName = "neon";
+}
