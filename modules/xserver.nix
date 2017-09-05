f6cc93117931cace0de05f5805317f839b4ab0ba
Tobias Happ <tobias.happ@gmx.de>
2017-09-05 23:53:32 +0200

    Removes unneccessary sudo extraConfig

--- modules/xserver.nix
+++ modules/xserver.nix
@@ -46,19 +46,7 @@
     };
   };
 
-  security = {
-    sudo.extraConfig = ''
-      Cmnd_Alias SYS_HALT = /run/current-system/sw/bin/systemctl halt
-      Cmnd_Alias SYS_HIBERNATE = /run/current-system/sw/bin/systemctl hibernat
-      Cmnd_Alias SYS_HYBRID = /run/current-system/sw/bin/systemctl hybrid-sleep
-      Cmnd_Alias SYS_REBOOT = /run/current-system/sw/bin/systemctl reboot
-      Cmnd_Alias SYS_SUSPEND = /run/current-system/sw/bin/systemctl suspend
-
-      tobias ALL= NOPASSWD: SYS_HALT, SYS_HIBERNATE, SYS_HYBRID, SYS_REBOOT, SYS_SUSPEND
-    '';
-
-    wrappers.slock.source = "${pkgs.slock}/bin/slock";
-  };
+  security.wrappers.slock.source = "${pkgs.slock}/bin/slock";
 
   services.xserver = {
     enable = true;
