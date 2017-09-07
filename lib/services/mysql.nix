6db0363330e70494c6d1b562364f4f4a2197df1a
Tobias Happ <tobias.happ@gmx.de>
2017-09-07 08:50:42 +0200

    Adds mysql service

---
+++ lib/services/mysql.nix
@@ -0,0 +1,44 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+let
+  cfg = config.custom.services.mysql;
+in
+
+{
+
+  ###### interface
+
+  options = {
+
+    custom.services.mysql = {
+
+      enable = mkOption {
+        type = types.bool;
+        default = false;
+        description = ''
+          Whether to install and configure mysql (MariaDB).
+        '';
+      };
+
+    };
+
+  };
+
+
+  ###### implementation
+
+  config = mkIf cfg.enable {
+
+    services.mysql = {
+      # set password with:
+      # SET PASSWORD FOR root@localhost = PASSWORD('password');
+      enable = true;
+      package = pkgs.mariadb;
+      dataDir = "/var/db/mysql";
+    };
+
+  };
+
+}
