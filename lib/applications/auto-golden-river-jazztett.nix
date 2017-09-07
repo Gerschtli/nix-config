26b3df9fe9f1b341546db8ceebb23ca136b92a5c
Tobias Happ <tobias.happ@gmx.de>
2017-09-07 09:10:56 +0200

    Adds applications

---
+++ lib/applications/auto-golden-river-jazztett.nix
@@ -0,0 +1,79 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+let
+  cfg = config.custom.applications.auto-golden-river-jazztett;
+  fetchBitBucket = import ../util/fetchBitBucket.nix pkgs;
+
+  autoGoldenRiverJazztett = pkgs.stdenv.mkDerivation rec {
+    name = "auto-golden-river-jazztett-${version}";
+    version = "2017-09-03";
+
+    src = fetchBitBucket {
+      url = "git@bitbucket.org:tobiashapp/auto-golden-river-jazztett.git";
+      rev = "82025349769f8c011791c212168eddd822d942c8";
+      sha256 = "0kvy60s1p3kxxk43i6pd4gka64plqd46dh2dfs24wmz57rhzh10s";
+    };
+
+    phases = [ "unpackPhase" "installPhase" "fixupPhase" ];
+
+    installPhase = "cp -R . \$out";
+
+    postFixup = ''
+      rm $out/dump.sql $out/install.sh
+    '';
+  };
+
+in
+
+{
+
+  ###### interface
+
+  options = {
+
+    custom.applications.auto-golden-river-jazztett = {
+
+      enable = mkOption {
+        type = types.bool;
+        default = false;
+        description = ''
+          Whether to install auto.goldenriverjazztett.de.
+        '';
+      };
+
+    };
+
+  };
+
+
+  ###### implementation
+
+  config = mkIf cfg.enable {
+
+    custom.services = {
+      httpd.enable = true;
+      mysql.enable = true;
+    };
+
+    services.httpd = {
+      enablePHP = true;
+      virtualHosts = [
+        {
+          hostName = "auto.goldenriverjazztett.de";
+          documentRoot = "${autoGoldenRiverJazztett}";
+          extraConfig = ''
+            <Directory ${autoGoldenRiverJazztett}>
+              Options -Indexes
+              DirectoryIndex index.php
+              AllowOverride All
+            </Directory>
+          '';
+        }
+      ];
+    };
+
+  };
+
+}
---
+++ lib/util/fetchBitBucket.nix
@@ -0,0 +1,19 @@
+{ pkgs, ... }:
+
+pkgs.callPackage (
+  builtins.scopedImport
+    {
+      __nixPath = [
+        {
+          path = pkgs.writeText "ssh_config" ''
+            Host bitbucket.org
+              IdentityFile /etc/nixos/keys/id_rsa.bitbucket-deploy
+              StrictHostKeyChecking no
+              UserKnownHostsFile=/dev/null
+          '';
+          prefix="ssh-config-file";
+        }
+      ] ++ __nixPath;
+    }
+    <nixpkgs/pkgs/build-support/fetchgit/private.nix>
+  ) { }
