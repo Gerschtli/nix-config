b3286a71acfd43645e5b35a5107cb3cd510592fa
Tobias Happ <tobias.happ@gmx.de>
2017-09-07 11:24:21 +0200

    Adds automatic optimise

--- lib/server.nix
+++ lib/server.nix
@@ -39,10 +39,17 @@ in
 
     networking.firewall.enable = true;
 
-    nix.gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 14d";
+    nix = {
+      gc = {
+        automatic = true;
+        dates = "Mon *-*-* 00:00:00";
+        options = "--delete-older-than 14d";
+      };
+
+      optimise = {
+        automatic = true;
+        dates = [ "Mon *-*-* 01:00:00" ];
+      };
     };
 
     services = {
