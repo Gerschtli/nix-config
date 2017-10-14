2a2a60166800c5d4df58bfe86f9ac00a73911204
Tobias Happ <tobias.happ@gmx.de>
2017-10-15 00:59:57 +0200

    Refactors spotify overlay

--- modules/overlays/spotify.nix
+++ modules/overlays/spotify.nix
@@ -1,7 +1,16 @@
 # update version because of unavailabilty
-import ./util/getPkgFromRev.nix {
-  package     = "spotify";
-  rev         = "cd490dc619dd435f0607d8bca3d7ef121d87525f";
-  sha256      = "12vch9favhbh62cpvbjxrprxws4z76jn2894q29mj0zym7qnapa8";
-  allowUnfree = true;
-}
+self: super:
+
+let
+  nixpkgs = import (super.fetchFromGitHub {
+    owner  = "NixOS";
+    repo   = "nixpkgs";
+    rev    = "cd490dc619dd435f0607d8bca3d7ef121d87525f";
+    sha256 = "12vch9favhbh62cpvbjxrprxws4z76jn2894q29mj0zym7qnapa8";
+  }) {
+    config   = { allowUnfree = true; };
+    overlays = [];
+  };
+in
+
+{ inherit (nixpkgs) spotify; }
