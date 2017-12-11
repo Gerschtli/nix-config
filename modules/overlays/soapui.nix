5b655b8333ec6baea95750e9810bbca660610d78
Tobias Happ <tobias.happ@gmx.de>
2017-12-11 23:51:52 +0100

    Updates soapui

--- modules/overlays/soapui.nix
+++ modules/overlays/soapui.nix
@@ -8,11 +8,11 @@ in
 {
   soapui = stdenv.mkDerivation rec {
     name = "soapui-${version}";
-    version = "5.3.0";
+    version = "5.4.0";
 
     src = fetchurl {
-      url = "https://b537910400b7ceac4df0-22e92613740a7dd1247910134033c0d1.ssl.cf5.rackcdn.com/soapui/${version}/SoapUI-${version}-linux-bin.tar.gz";
-      sha256 = "1x270zs9947jdpg9wziiyfh147i1ly535wg0wxfiayr6x3fwlakd";
+      url = "https://s3.amazonaws.com/downloads.eviware/soapuios/${version}/SoapUI-${version}-linux-bin.tar.gz";
+      sha256 = "1yqx1fsh8mr5zf36df7pi25dysb28gfscr1667jzd5s0k9jl42xd";
     };
 
     buildInputs = [ jdk maven ];
@@ -26,7 +26,7 @@ in
       (writeText "soapui-${version}.patch" ''
         --- a/bin/soapui.sh
         +++ b/bin/soapui.sh
-        @@ -34,7 +34,7 @@ SOAPUI_CLASSPATH=$SOAPUI_HOME/bin/soapui-5.3.0.jar:$SOAPUI_HOME/lib/*
+        @@ -34,7 +34,7 @@ SOAPUI_CLASSPATH=$SOAPUI_HOME/bin/soapui-${version}.jar:$SOAPUI_HOME/lib/*
          export SOAPUI_CLASSPATH
 
          JAVA_OPTS="-Xms128m -Xmx1024m -XX:MinHeapFreeRatio=20 -XX:MaxHeapFreeRatio=40 -Dsoapui.properties=soapui.properties -Dsoapui.home=$SOAPUI_HOME/bin -splash:SoapUI-Spashscreen.png"
