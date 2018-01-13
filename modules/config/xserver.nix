88fd986aacc94638e502f9ec96280bc87c65a483
Tobias Happ <tobias.happ@gmx.de>
2018-01-14 00:00:50 +0100

    Disable caps lock

--- modules/config/xserver.nix
+++ modules/config/xserver.nix
@@ -63,6 +63,7 @@ in
       services.xserver = {
         enable = true;
         layout = "de";
+        xkbOptions = "ctrl:nocaps";
         xkbVariant = "nodeadkeys";
 
         desktopManager.xterm.enable = false;
