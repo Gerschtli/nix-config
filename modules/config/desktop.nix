8268d3de06b2bd1d9f1c2836f39992b0d214d1aa
Tobias Happ <tobias.happ@gmx.de>
2018-05-19 20:28:19 +0200

    Merges desktop.nix and xserver.nix

--- modules/config/desktop.nix
+++ modules/config/desktop.nix
@@ -1,9 +1,11 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, ... } @ args:
 
 with lib;
 
 let
   cfg = config.custom.desktop;
+
+  customLib = import ../lib args;
 in
 
 {
@@ -37,31 +39,133 @@ in
 
   ###### implementation
 
-  config = mkIf cfg.enable {
+  config = mkIf cfg.enable (mkMerge [
+    {
 
-    custom = {
-      applications.pass = {
-        enable = true;
-        browserpass = true;
+      custom = {
+        applications.pass = {
+          enable = true;
+          browserpass = true;
+        };
+
+        boot.isEFI = true;
+
+        dev.enable = true;
       };
 
-      boot.isEFI = true;
+      environment.systemPackages = with pkgs; [
+        exfat
+        ntfs3g
+      ];
 
-      dev.enable = true;
-
-      xserver = {
-        enable = true;
-        laptop = cfg.laptop;
+      fonts = {
+        enableFontDir = true;
+        enableGhostscriptFonts = true;
+        fonts = with pkgs; [
+          corefonts
+          dejavu_fonts
+          fira-code
+          fira-mono
+          google-fonts
+          inconsolata
+          ipafont
+          nerdfonts
+          powerline-fonts
+          source-code-pro
+          terminus_font
+          ubuntu_font_family
+          unifont
+        ];
       };
-    };
 
-    environment.systemPackages = with pkgs; [
-      exfat
-      ntfs3g
-    ];
+      hardware.pulseaudio.enable = true;
 
-    sound.enable = true;
+      nixpkgs = {
+        config.allowUnfree = true;
 
-  };
+        overlays = map (file: import file) (customLib.getRecursiveFileList ../overlays);
+      };
+
+      programs = {
+        slock.enable = true;
+
+        ssh.askPassword = "";
+      };
+
+      services.xserver = {
+        enable = true;
+        layout = "de";
+        xkbOptions = "ctrl:nocaps";
+        xkbVariant = "nodeadkeys";
+
+        desktopManager.xterm.enable = false;
+
+        displayManager.slim = {
+          enable = true;
+          defaultUser = "tobias";
+          extraConfig = "numlock on";
+        };
+
+        windowManager.dwm.enable = true;
+      };
+
+      sound.enable = true;
+
+      users.users.tobias.packages = with pkgs; [
+        dmenu
+        dunst
+        gnome3.zenity
+        libnotify # for notify-send
+        pavucontrol
+        wmname
+        xclip
+        xss-lock
+        xterm
+
+        atom
+        eclipses.eclipse-sdk
+        gimp
+        google-chrome
+        libreoffice
+        qpdfview
+        soapui
+        spotify
+        sublime3
+        thunderbird
+      ];
+    }
+
+    (mkIf cfg.laptop
+      {
+        environment.systemPackages = with pkgs; [
+          # install globally because of icons not found
+          networkmanagerapplet
+        ];
+
+        networking.networkmanager.enable = true;
+
+        services = {
+          logind.extraConfig = ''
+            HandlePowerKey=ignore
+          '';
+
+          upower.enable = true;
+
+          xserver.libinput = {
+            enable = true;
+            accelProfile = "flat";
+            additionalOptions = ''
+              Option "TappingButtonMap" "lmr"
+            '';
+          };
+        };
+
+        users.users.tobias.packages = with pkgs; [
+          xorg.xbacklight
+        ];
+      }
+    )
+
+  ]);
 
 }
