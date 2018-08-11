c5bd1295c74de6ba8d365934e582e93c0d190f22
Tobias Happ <tobias.happ@gmx.de>
2018-08-11 10:34:06 +0200

    Disables teamspeak on neon

--- configuration-neon.nix
+++ configuration-neon.nix
@@ -6,14 +6,10 @@
   custom = {
     applications.golden-river-jazztett.enable = true;
 
-    backup.enable = true;
-
     server = {
       enable = true;
       ipv6Address = "2a01:4f8:1c0c:7168::2";
     };
-
-    services.teamspeak.enable = true;
   };
 
   networking.hostName = "neon";
