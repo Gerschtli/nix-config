8b5971e202eeba81a65afc01c805dad3557b454c
Tobias Happ <tobias.happ@gmx.de>
2018-08-11 10:58:01 +0200

    Changes default value of backup module to false

--- modules/config/backup.nix
+++ modules/config/backup.nix
@@ -81,7 +81,7 @@ in
 
       enable = mkOption {
         type = types.bool;
-        default = true;
+        default = false;
         description = ''
           Whether to enable backups.
         '';
