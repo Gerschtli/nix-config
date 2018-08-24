5bb586856ae91195c6b51a1912d819f3509e0e69
Tobias Happ <tobias.happ@gmx.de>
2018-08-24 15:31:52 +0200

    Updates teamspeak-update-notifier

--- modules/overlays/teamspeak-update-notifier.nix
+++ modules/overlays/teamspeak-update-notifier.nix
@@ -3,24 +3,41 @@ self: super:
 let
   inherit (self) python36 python36Packages;
   inherit (super) fetchFromGitHub;
+
+  dependency_injector = python36Packages.buildPythonPackage rec {
+    pname = "dependency_injector";
+    version = "3.13.1";
+
+    src = fetchFromGitHub {
+      owner = "ets-labs";
+      repo = pname;
+      rev = version;
+      sha256 = "03cn4aanz6q28vy5946bpbmnpg25szzp0q3m4wk82ardw5j6x07i";
+    };
+
+    doCheck = false;
+
+    propagatedBuildInputs = with python36Packages; [ six ];
+  };
 in
 
 {
   teamspeak-update-notifier = python36.pkgs.buildPythonPackage rec {
     pname = "teamspeak-update-notifier";
-    version = "1.2.0";
+    version = "1.3.1";
 
     src = fetchFromGitHub {
       owner = "Gerschtli";
       repo = "teamspeak-update-notifier";
       rev = version;
-      sha256 = "0wdq666zs7llc4qg9730i93yszlq20qzglhcynd1r7jg449z7ghz";
+      sha256 = "1hrvw8q928cnqxbhz1dl550g6zy33agwxgbf4vk6yj04j72x141v";
     };
 
     doCheck = false;
 
     propagatedBuildInputs = with python36Packages; [
       beautifulsoup4
+      dependency_injector
       requests
     ];
   };
