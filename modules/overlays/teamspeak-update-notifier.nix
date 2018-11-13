bf4db3ad6f16f155748c9fa7a3accfae61fbf32c
Tobias Happ <tobias.happ@gmx.de>
2018-11-13 22:10:13 +0100

    Remove dependency_injector in overlay

--- modules/overlays/teamspeak-update-notifier.nix
+++ modules/overlays/teamspeak-update-notifier.nix
@@ -3,22 +3,6 @@ self: super:
 let
   inherit (self) python36 python36Packages;
   inherit (super) fetchFromGitHub;
-
-  dependency_injector = python36Packages.buildPythonPackage rec {
-    pname = "dependency_injector";
-    version = "3.13.1";
-
-    src = fetchFromGitHub {
-      owner = "ets-labs";
-      repo = pname;
-      rev = version;
-      sha256 = "03cn4aanz6q28vy5946bpbmnpg25szzp0q3m4wk82ardw5j6x07i";
-    };
-
-    doCheck = false;
-
-    propagatedBuildInputs = with python36Packages; [ six ];
-  };
 in
 
 {
