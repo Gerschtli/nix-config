faf7268c912d0a879a6a3fa60e4fededcddd4688
Tobias Happ <tobias.happ@gmx.de>
2018-12-01 17:16:30 +0100

    Update dwm-status overlay

--- modules/overlays/dwm-status.nix
+++ modules/overlays/dwm-status.nix
@@ -1,13 +1,13 @@
 self: super:
 
 let
-  # PR branch to update dwm-status to 1.4.0
+  # PR branch to update dwm-status to 1.4.1
   pkgs = let
     pinnedPkgs = super.fetchFromGitHub {
       owner = "Gerschtli";
       repo = "nixpkgs";
-      rev = "6760af490e5e9e99dbdd80967055c83e91c0c457";
-      sha256 = "0cdwhgsfy88h97gwn7c130gkbf9lj85mzf4xhqqps0npfkscqpn0";
+      rev = "bc2d26fd8f5f0e179cb739817b220bfa81da950e";
+      sha256 = "00wagg5c6vsyk8fvb8wcv1y5mg1y6qd35qpzs03n86csq8sqdr2b";
     };
   in import pinnedPkgs { overlays = []; };
 in
