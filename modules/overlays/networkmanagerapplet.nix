d17ca9da1b0a74fca07a99fec74e10ae025b266a
Tobias Happ <tobias.happ@gmx.de>
2018-12-07 23:37:28 +0100

    Remove obsolete overlay

--- modules/overlays/networkmanagerapplet.nix
+++
@@ -1,9 +0,0 @@
-# FIXME: remove when https://github.com/NixOS/nixpkgs/commit/49be1fad9b737b201e7cbebb3221d0e3b081c3af gets merged
-
-self: super:
-
-{
-  networkmanagerapplet = super.networkmanagerapplet.overrideDerivation (old: {
-    buildInputs = old.buildInputs ++ [ self.gnome3.defaultIconTheme ];
-  });
-}
