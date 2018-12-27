0ee9a7598ddec79fe85ae3e3165c86c53cce5aa6
Tobias Happ <tobias.happ@gmx.de>
2018-12-27 19:19:18 +0100

    Update dwm-status overlay

--- modules/overlays/dwm-status.nix
+++ modules/overlays/dwm-status.nix
@@ -1,15 +1,15 @@
-# FIXME: remove when https://github.com/NixOS/nixpkgs/pull/52716 got merged
+# FIXME: remove when https://github.com/NixOS/nixpkgs/pull/52982 got merged
 
 self: super:
 
 let
-  # PR branch to update dwm-status to 1.5.0
+  # PR branch to update dwm-status to 1.6.0
   pkgs = let
     pinnedPkgs = super.fetchFromGitHub {
       owner = "Gerschtli";
       repo = "nixpkgs";
-      rev = "a08b98f0c17650da609e4331f5058ca2177471af";
-      sha256 = "009k2f00dma2441rcghci3dsglar30skjlfjz45cd19inb9b4d3x";
+      rev = "6621b92ad145568c57bfaf7b339bba6b65b1001b";
+      sha256 = "1f507lvnik2n8naz6hchf8vbj6x5sc4nyhawi949bhw21b6ycd3v";
     };
   in import pinnedPkgs { overlays = []; };
 in
