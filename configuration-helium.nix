eddf79413b8d4eb3ad22263276ff513497919adc
Tobias Happ <tobias.happ@gmx.de>
2019-01-28 20:05:16 +0100

    Configure xrandrHeads for helium

--- configuration-helium.nix
+++ configuration-helium.nix
@@ -14,7 +14,18 @@
 
   hardware.opengl.driSupport32Bit = true;
 
-  services.xserver.videoDrivers = [ "nvidia" ];
+  services.xserver = {
+    videoDrivers = [ "nvidia" ];
+
+    # Toggle HDMI-0 via:
+    # xrandr --output "HDMI-0" --left-of "DVI-I-1" --auto
+    # xrandr --output "HDMI-0" --off
+    xrandrHeads = [
+      { output = "HDMI-0";  monitorConfig = ''Option "Enable" "false"''; }
+      { output = "DVI-I-1"; primary = true; }
+      { output = "DVI-D-0"; }
+    ];
+  };
 
   networking.hostName = "helium";
 }
