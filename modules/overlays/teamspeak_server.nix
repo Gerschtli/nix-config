dee3e225e4c28b713bcf479001a6e2fb802af623
Tobias Happ <tobias.happ@gmx.de>
2019-03-04 15:39:55 +0100

    Update teamspeak_server overlay

--- modules/overlays/teamspeak_server.nix
+++ modules/overlays/teamspeak_server.nix
@@ -1,11 +1,9 @@
-# FIXME: remove when https://github.com/NixOS/nixpkgs/pull/54547 gets merged
-
 self: super:
 
 let
   inherit (super) fetchurl stdenv;
 
-  version = "3.5.1";
+  version = "3.6.1";
   arch = if stdenv.is64bit then "amd64" else "x86";
 in
 
@@ -19,8 +17,8 @@ in
         "http://teamspeak.gameserver.gamed.de/ts3/releases/${version}/teamspeak3-server_linux_${arch}-${version}.tar.bz2"
       ];
       sha256 = if stdenv.is64bit
-        then "0ygb867ff2fvi9n9hgs4hldpg4y012w4i1d9cx4f5mpli1xim6da"
-        else "0g1cixsldpdbfzg2vain7h3hr5j3xjdngjw66r0aqnzbx743gjzj";
+        then "0wgnb7fdy393anridymm1frlgr86j0awxnzvd498ar5fch06ij87"
+        else "0x6p1z4qvgy464n6gnkaqrm7dns1ysyadm68sr260d39a7028q1c";
     };
   });
 }
