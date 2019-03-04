1c7f73027354534106d95e395f23688a734e980a
Tobias Happ <tobias.happ@gmx.de>
2019-03-04 15:40:20 +0100

    Update xrandrHeads for new monitor config

--- configuration-helium.nix
+++ configuration-helium.nix
@@ -17,9 +17,13 @@
   services.xserver = {
     videoDrivers = [ "nvidia" ];
 
+    # Toggle DVI-D-0 via:
+    # xrandr --output "DVI-D-0" --left-of "HDMI-0" --auto
+    # xrandr --output "DVI-D-0" --off
     xrandrHeads = [
+      { output = "DVI-D-0";  monitorConfig = ''Option "Enable" "false"''; }
       { output = "HDMI-0"; primary = true; }
-      { output = "DVI-D-0"; }
+      { output = "DVI-I-1"; }
     ];
   };
 
