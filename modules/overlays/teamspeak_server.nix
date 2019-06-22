4993a456c82dc9ddea538134c3b0199a2516b0f6
Tobias Happ <tobias.happ@gmx.de>
2019-06-22 17:34:54 +0200

    Update teamspeak server overlay

--- modules/overlays/teamspeak_server.nix
+++ modules/overlays/teamspeak_server.nix
@@ -3,8 +3,9 @@ self: super:
 let
   inherit (super) fetchurl stdenv;
 
-  version = "3.7.1";
+  version = "3.8.0";
   arch = if stdenv.is64bit then "amd64" else "x86";
+  libDir = if stdenv.is64bit then "lib64" else "lib";
 in
 
 {
@@ -17,8 +18,26 @@ in
         "http://teamspeak.gameserver.gamed.de/ts3/releases/${version}/teamspeak3-server_linux_${arch}-${version}.tar.bz2"
       ];
       sha256 = if stdenv.is64bit
-        then "1w60241zsvr8d1qlkca6a1sfxa1jz4w1z9kjd0wd2wkgzp4x91v7"
-        else "0s835dnaw662sb2v5ahqiwry0qjcpl7ff9krnhbw2iblsbqis3fj";
+        then "1bzmqqqpwn6q2pvkrkkxq0ggs8crxbkwaxlggcdxjlyg95cyq8k1"
+        else "0p5rqwdsvbria5dzjjm5mj8vfy0zpfs669wpbwxd4g3n4vh03kyw";
     };
+
+    installPhase = ''
+      # Delete unecessary libraries - these are provided by nixos.
+      #rm *.so*
+
+      # Install files.
+      mkdir -p $out/lib/teamspeak
+      mv * $out/lib/teamspeak/
+
+      # Make symlinks to the binaries from bin.
+      mkdir -p $out/bin/
+      ln -s $out/lib/teamspeak/ts3server $out/bin/ts3server
+      ln -s $out/lib/teamspeak/tsdnsserver $out/bin/tsdnsserver
+
+      wrapProgram $out/lib/teamspeak/ts3server \
+        --prefix LD_LIBRARY_PATH : ${stdenv.cc.cc.lib}/${libDir} \
+        --prefix LD_LIBRARY_PATH : $out/lib/teamspeak
+    '';
   });
 }
