904eb102d0c12b0f3d8a7e6c65d0884eb4704e29
Tobias Happ <tobias.happ@gmx.de>
2019-07-24 00:09:44 +0200

    Migrate shell dotfiles module

--- modules/config/shell.nix
+++ modules/config/shell.nix
@@ -4,6 +4,61 @@ with lib;
 
 let
   cfg = config.custom.shell;
+
+  initExtra = ''
+    eval "$(dircolors -b)"
+  '';
+
+  logoutExtra = ''
+    [[ -e "$HOME/.lesshst" ]]             && rm -f "$HOME/.lesshst"
+    [[ -e "$HOME/.xsession-errors.old" ]] && rm -f "$HOME/.xsession-errors.old"
+    [[ -e "$HOME/.zcompdump" ]]           && rm -f "$HOME/.zcompdump"*
+
+    ${pkgs.ncurses}/bin/clear
+  '';
+
+  profileExtra = ''
+    umask 022
+  '';
+
+  shellAliases = {
+    ls = "ls --color=auto";
+    la = "ls -AFv";
+    ll = "ls -AFhlv";
+
+    cp = "cp -av";
+    mv = "mv -v";
+    rm = "rm -v";
+    ln = "ln -v";
+
+    grep = "grep --color=auto";
+    fgrep = "fgrep --color=auto";
+    egrep = "egrep --color=auto";
+
+    sort-vn = "sort -t. -k 1,1n -k 2,2n -k 3,3n -k 4,4n";
+
+    ".." = "cd ..";
+    "..." = "cd ../..";
+    "...." = "cd ../../..";
+    "....." = "cd ../../../..";
+    "......" = "cd ../../../../..";
+
+    e = "$EDITOR";
+
+    bc = "bc -l";
+
+    df = "df -h";
+
+    rg = "rg --ignore-case --sort=path";
+
+    open = "xdg-open";
+
+    pwgen = "pwgen -cny";
+    pwgens = "pwgen -s";
+
+    tree = "tree -F --dirsfirst";
+    treea = "tree -a";
+  };
 in
 
 {
@@ -27,8 +82,11 @@ in
 
     programs = {
       bash = {
+        inherit initExtra logoutExtra profileExtra shellAliases;
       };
+
       zsh = {
+        inherit initExtra logoutExtra profileExtra shellAliases;
       };
     };
 
