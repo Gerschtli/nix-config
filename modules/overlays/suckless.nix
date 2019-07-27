db0f55f8469addecf4db26e0db5e29f19eee696f
Tobias Happ <tobias.happ@gmx.de>
2019-07-27 13:37:28 +0200

    Remove suckless overlay

--- modules/overlays/suckless.nix
+++
@@ -1,7 +0,0 @@
-self: super:
-
-super.lib.genAttrs
-  [ "dmenu" "dwm" "slock" ]
-  (name: super.${name}.overrideDerivation (old: {
-    patches = [ (../../patches + "/${name}-config.diff") ];
-  }))
