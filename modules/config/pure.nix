9f891c516ec1de2b08fa3c50f04e3b3ab8a2acc8
Tobias Happ <tobias.happ@gmx.de>
2019-07-29 22:06:07 +0200

    Add pure module

---
+++ modules/config/pure.nix
@@ -0,0 +1,60 @@
+{ config, lib, pkgs, ... } @ args:
+
+with lib;
+
+let
+  cfg = config.custom.pure;
+
+  # TODO: add in nixpkgs
+  pure = pkgs.stdenv.mkDerivation {
+    name = "pure";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "sindresorhus";
+      repo = "pure";
+      rev = "v1.10.3";
+      sha256 = "0zjgnlw01ri0brx108n6miw4y0cxd6al1bh28m8v8ygshm94p1zx";
+    };
+
+    installPhase = ''
+      mkdir -p $out/share/pure
+
+      install -m 0644 pure.zsh $out/share/pure/prompt_pure_setup
+      install -m 0644 async.zsh $out/share/pure/async
+    '';
+  };
+in
+
+{
+
+  ###### interface
+
+  options = {
+
+    custom.pure.enable = mkEnableOption "pure prompt";
+
+  };
+
+
+  ###### implementation
+
+  config = mkIf cfg.enable {
+
+    # TODO: add as module to home-manager
+    programs.zsh = {
+      initExtra = ''
+        autoload -U promptinit; promptinit
+        prompt pure
+      '';
+
+      plugins = [
+        {
+          name = "pure";
+          src = "${pure}/share/pure";
+        }
+      ];
+    };
+
+  };
+
+}
