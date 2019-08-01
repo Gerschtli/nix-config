5ee91721b324d8be9817c98bfcd0f1ab09e27754
Tobias Happ <tobias.happ@gmx.de>
2019-08-01 20:03:00 +0200

    Remove debug statements

--- modules/files/git/hooks/scripts/permissions.sh
+++ modules/files/git/hooks/scripts/permissions.sh
@@ -6,9 +6,7 @@ SECRET_FILES="${PWD}/.secret-files"
 
 install() {
     cat "${SECRET_FILES}" | while read -r line; do
-        echo $line
         if [[ -e "${PWD}/${line}" ]]; then
-            echo chmod
             chmod 0600 "${PWD}/${line}"
         fi
     done
