b8b1451ea39e744fcf97be9dbddee84d6fdccd42
Tobias Happ <tobias.happ@gmx.de>
2019-08-01 20:19:03 +0200

    Add keychain setup

--- modules/config/ssh.nix
+++ modules/config/ssh.nix
@@ -28,35 +28,100 @@ in
 
   config = mkIf cfg.enable {
 
-    programs.ssh = {
-      enable = true;
+    custom.shell = {
+      initExtra = ''
+        keygen() {
+          if [[ -z "$1" ]]; then
+            echo "Enter path  as argument!"
+          else
+            ssh-keygen -t rsa -b 4096 -f "$1"
+          fi
+        }
 
-      compression = true;
-      serverAliveInterval = 30;
-      hashKnownHosts = true;
-      controlMaster = "auto";
-      controlPath = "~/.ssh/socket-%r@%h-%p";
-      controlPersist = "10m";
+        kadd() {
+          local key="$(find "${directoryDestination}" -type f -name "id_rsa.$1" | head -n 1)"
 
-      extraConfig = ''
-        CheckHostIP yes
-        ConnectTimeout 60
-        EnableSSHKeysign yes
-        ExitOnForwardFailure yes
-        ForwardX11Trusted yes
-        IdentitiesOnly yes
-        NoHostAuthenticationForLocalhost yes
-        Protocol 2
-        PubKeyAuthentication yes
-        SendEnv LANG LC_*
-        ServerAliveCountMax 30
+          if [[ ! -r "$key" ]]; then
+            echo "ssh key not found: $key"
+          else
+            keychain "$key"
+          fi
+
+          if [[ $# > 1 ]]; then
+            kadd "''${@:2}"
+          fi
+        }
       '';
 
-      matchBlocks = mkMerge (
-        map
-          (module: (import module { path = "${toString module}"; }).matchBlocks)
-          modules
-      );
+      profileExtra = ''
+        # remove existing keys
+        if [[ $SHLVL == 1 ]]; then
+          keychain --clear --quiet
+        fi
+      '';
+    };
+
+    programs = {
+      keychain = {
+        enable = true;
+        enableBashIntegration = true;
+        enableZshIntegration = true;
+        agents = [ "ssh" ];
+      };
+
+      ssh = {
+        enable = true;
+
+        compression = true;
+        serverAliveInterval = 30;
+        hashKnownHosts = true;
+        controlMaster = "auto";
+        controlPath = "~/.ssh/socket-%r@%h-%p";
+        controlPersist = "10m";
+
+        extraConfig = ''
+          CheckHostIP yes
+          ConnectTimeout 60
+          EnableSSHKeysign yes
+          ExitOnForwardFailure yes
+          ForwardX11Trusted yes
+          IdentitiesOnly yes
+          NoHostAuthenticationForLocalhost yes
+          Protocol 2
+          PubKeyAuthentication yes
+          SendEnv LANG LC_*
+          ServerAliveCountMax 30
+        '';
+
+        matchBlocks = mkMerge (
+          map
+            (module: (import module { path = "${toString module}"; }).matchBlocks)
+            modules
+        );
+      };
+
+      zsh.plugins = [
+        {
+          name = "kadd completion";
+          src = pkgs.writeTextDir "_kadd" ''
+            #compdef kadd
+
+            LIST=()
+
+            for module in "${directoryDestination}/"*; do
+              prefix="$module/keys/id_rsa."
+              suffix=".pub"
+
+              for keyfile in "$prefix"*"$suffix"; do
+                  tmp="''${keyfile#$prefix}"
+                  LIST=("''${tmp//$suffix}" "$LIST")
+              done
+            done
+
+            _arguments "*:ssh keys:($LIST)"
+          '';
+        }
+      ];
     };
 
     systemd.user.services.install-ssh-keys = {
