2230044936cd42cc2a6316fd3644e29df2b810ad
Tobias Happ <tobias.happ@gmx.de>
2019-08-03 21:24:18 +0200

    Remove systemPackages as these are installed via home-manager

--- modules/config/base/general.nix
+++ modules/config/base/general.nix
@@ -38,43 +38,7 @@ in
 
     custom.system.firewall.enable = true;
 
-    environment = {
-      shellAliases = mkForce { };
-
-      systemPackages = with pkgs; [
-        bc
-        file
-        fzf
-        git
-        gitAndTools.tig
-        htop
-        httpie
-        iotop
-        keychain
-        nox
-        pwgen
-        ripgrep
-        tmux
-        tree
-        wget
-
-        gzip
-        unzip
-        xz
-        zip
-
-        bind # dig
-        netcat
-        psmisc # killall
-        whois
-
-        ( # FIXME: use always neovim after https://github.com/NixOS/nixpkgs/issues/45026 got fixed
-          if stdenv.hostPlatform.system == "aarch64-linux"
-          then vim
-          else neovim
-        )
-      ];
-    };
+    environment.shellAliases = mkForce { };
 
     home-manager.useUserPackages = true;
 
