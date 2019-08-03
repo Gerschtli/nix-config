46b532d187438e4cb6af12346e15d700c0608213
Tobias Happ <tobias.happ@gmx.de>
2019-08-03 23:59:55 +0200

    Remove util/pinned-pkgs.nix

--- modules/overlays/php55.nix
+++ modules/overlays/php55.nix
@@ -3,11 +3,14 @@ self: super:
 let
   # one commit before php 5.6 drop
   # https://github.com/NixOS/nixpkgs/commit/7e6b76fc6bdebb12c8c3c2e6223f9fe5f3b38a39#diff-e3f1a5e8a80452693ffb5de49ae02c5b
-  pkgs = import ./util/pinned-pkgs.nix {
-    inherit (super) fetchFromGitHub;
-
+  pkgs = import (super.fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs";
     rev = "846d8f8305192dcc3a63139102698b4ac6b9ef9f";
     sha256 = "1qifgc1q2i4g0ivpfjnxp4jl2cc82gfjws08dsllgw7q7kw4b4rb";
+  }) {
+    config = { };
+    overlays = [ ];
   };
 in
 
