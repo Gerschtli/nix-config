6ed4b3078e1e3d57570910fc2bb96b7fca60a3cb
Tobias Happ <tobias.happ@gmx.de>
2019-08-03 22:17:17 +0200

    Manage nix-channels via home-manager

--- home-files/argon/root.nix
+++ home-files/argon/root.nix
@@ -2,4 +2,9 @@
 
 {
   imports = [ ../../modules ];
+
+  nix-channels = {
+    nixos = "https://nixos.org/channels/nixos-19.03";
+    home-manager = "https://github.com/Gerschtli/home-manager/archive/local.tar.gz";
+  };
 }
