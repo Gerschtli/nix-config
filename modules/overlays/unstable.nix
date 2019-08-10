f5cc1f3f25f7d064fc6989f28fcecb8188e77e80
Tobias Happ <tobias.happ@gmx.de>
2019-08-10 19:05:22 +0200

    Add neovim-unwrapped in unstable packages

--- modules/overlays/unstable.nix
+++ modules/overlays/unstable.nix
@@ -11,6 +11,7 @@ in
   inherit (unstable)
     jetbrains         # need bleeding edge version
     neovim            # did not built on aarch64-linux, wait for https://github.com/NixOS/nixpkgs/pull/61588
+    neovim-unwrapped  # did not built on aarch64-linux, wait for https://github.com/NixOS/nixpkgs/pull/61588
     postman           # src did not exist anymore, wait for https://github.com/NixOS/nixpkgs/pull/58944
     teamspeak_server  # need bleeding edge version
   ;
