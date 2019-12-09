c91fd2932988a2028acb954096c2dc5ab54a561a
Tobias Happ <tobias.happ@gmx.de>
2019-12-09 18:09:21 +0100

    Add option for useGlobalAlsaUtils for dwm-status

--- modules/config/services/dwm-status.nix
+++ modules/config/services/dwm-status.nix
@@ -20,6 +20,8 @@ in
 
       laptop = mkEnableOption "laptop config";
 
+      useGlobalAlsaUtils = mkEnableOption "use global alsa utils instead of nix' one";
+
     };
 
   };
@@ -32,7 +34,10 @@ in
     services.dwm-status = {
       enable = true;
 
-      package = pkgs.nur-gerschtli.dwm-status;
+      package =
+        if cfg.useGlobalAlsaUtils
+        then pkgs.nur-gerschtli.dwm-status-without-alsa-utils
+        else pkgs.nur-gerschtli.dwm-status;
 
       order =
         let
