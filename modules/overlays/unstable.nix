d335fddfe3c7fb5950e076facd333bae1a121e92
Tobias Happ <tobias.happ@gmx.de>
2020-01-06 10:20:50 +0100

    Remove unstable vimplugins

--- modules/overlays/unstable.nix
+++ modules/overlays/unstable.nix
@@ -12,9 +12,4 @@ in
     jetbrains         # need bleeding edge version
     teamspeak_server  # need bleeding edge version
   ;
-
-  # do not exist in stable yet
-  vimPlugins = (super.vimPlugins or {}) // {
-    inherit (unstable.vimPlugins) gitignore-vim vim-hybrid-material vim-tmux;
-  };
 }
