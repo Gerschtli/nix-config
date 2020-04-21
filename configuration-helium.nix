4ccf56f423ad15c88161af52f8cc423f76f7d0a2
Tobias Happ <tobias.happ@gmx.de>
2020-04-21 23:26:45 +0200

    Clean up xrandrHeads for helium

--- configuration-helium.nix
+++ configuration-helium.nix
@@ -21,11 +21,7 @@
   services.xserver = {
     videoDrivers = [ "nvidia" ];
 
-    # Toggle DVI-D-0 via:
-    # xrandr --output "DVI-D-0" --left-of "HDMI-0" --auto
-    # xrandr --output "DVI-D-0" --off
     xrandrHeads = [
-      { output = "DVI-D-0";  monitorConfig = ''Option "Enable" "false"''; }
       { output = "HDMI-0"; primary = true; }
       { output = "DVI-I-1"; }
     ];
