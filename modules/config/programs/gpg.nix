c3a3a492cab89c5f6ada087d51ea48d686d46597
Tobias Happ <tobias.happ@gmx.de>
2020-04-26 17:06:56 +0200

    Add gpg module

---
+++ modules/config/programs/gpg.nix
@@ -0,0 +1,52 @@
+{ config, lib, pkgs, ... } @ args:
+
+with lib;
+
+let
+  cfg = config.custom.programs.gpg;
+in
+
+{
+
+  ###### interface
+
+  options = {
+
+    custom.programs.gpg = {
+      enable = mkEnableOption "gpg config";
+
+      curses = mkEnableOption "pinentry curses";
+    };
+
+  };
+
+
+  ###### implementation
+
+  config = mkIf cfg.enable {
+
+    custom = {
+      misc.dotfiles = {
+        enable = true;
+        modules = [ "gpg" ];
+      };
+
+      programs.shell.loginExtra = ''
+        # remove existing keys
+        if [[ $SHLVL -eq 1 ]]; then
+          ${pkgs.procps}/bin/pkill -SIGHUP gpg-agent
+        fi
+      '';
+    };
+
+    programs.gpg.enable = true;
+
+    services.gpg-agent = {
+      enable = true;
+      defaultCacheTtl = 300;
+      pinentryFlavor = mkIf cfg.curses "curses";
+    };
+
+  };
+
+}
