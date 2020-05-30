387d3771c16fb7d39b1c997c4df77e0ad3e3b2fb
Tobias Happ <tobias.happ@gmx.de>
2020-05-30 12:27:43 +0200

    Disable noXlibs for krypton

--- configuration-krypton.nix
+++ configuration-krypton.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
 {
   imports = [ ./modules ];
@@ -55,4 +55,7 @@
 
     system.boot.mode = "grub";
   };
+
+  # FIXME: pinentry needs gtk, this prevents recompiling it
+  environment.noXlibs = lib.mkForce false;
 }
