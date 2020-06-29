71079763de0f689c1803d67adfaf5cf48a98d0a4
Tobias Happ <tobias.happ@gmx.de>
2020-06-29 09:11:43 +0200

    Add FIXME's for autorandr config

--- home-files/radon/tobias.nix
+++ home-files/radon/tobias.nix
@@ -113,12 +113,16 @@ with lib;
               DVI-I-1-1 = {
                 enable = true;
                 primary = true;
+                # FIXME: add when https://github.com/rycee/home-manager/pull/1283 gets merged into stable
+                #crtc = 0;
                 mode = "2560x1440";
                 position = "0x0";
                 rate = "59.95";
               };
 
               eDP-1 = {
+                # FIXME: add when https://github.com/rycee/home-manager/pull/1283 gets merged into stable
+                #crtc = 1;
                 mode = "1368x768";
                 position = "2560x0";
                 rate = "59.88";
@@ -161,6 +165,8 @@ with lib;
               DVI-I-1-1 = {
                 enable = true;
                 primary = true;
+                # FIXME: add when https://github.com/rycee/home-manager/pull/1283 gets merged into stable
+                #crtc = 0;
                 mode = "2560x1440";
                 position = "0x0";
                 rate = "59.95";
@@ -178,6 +184,8 @@ with lib;
             config = mkConfigObject {
               eDP-1 = {
                 primary = true;
+                # FIXME: add when https://github.com/rycee/home-manager/pull/1283 gets merged into stable
+                #crtc = 0;
                 mode = "1920x1080";
                 position = "0x0";
                 rate = "59.96";
