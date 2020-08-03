1caad7301c671610f859298f9086246b49f65f15
Tobias Happ <tobias.happ@gmx.de>
2020-08-03 23:10:28 +0200

    Add FIXME for xenon kernel 4.19

--- modules/config/system/boot.nix
+++ modules/config/system/boot.nix
@@ -76,6 +76,7 @@ in
             "vm.dirty_ratio" = 80;
           };
 
+          # FIXME: use default or latest as soon as https://github.com/NixOS/nixpkgs/issues/82455 gets resolved
           kernelPackages = pkgs.linuxPackages_4_19;
           kernelParams = ["cma=32M"];
         };
