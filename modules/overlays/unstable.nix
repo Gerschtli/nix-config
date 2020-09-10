7dae7630dcfe5eaa210ce20e3e8e75200d02f49b
Tobias Happ <tobias.happ@gmx.de>
2020-09-10 22:58:36 +0200

    Use liquidprompt of unstable

--- modules/overlays/unstable.nix
+++ modules/overlays/unstable.nix
@@ -10,6 +10,7 @@ in
 {
   inherit (unstable)
     jetbrains         # need bleeding edge version
+    liquidprompt      # need version 1.12.0
     teamspeak_server  # need bleeding edge version
     terraform         # need bleeding edge version
   ;
