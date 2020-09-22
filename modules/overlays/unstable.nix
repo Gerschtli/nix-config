74c0f404e8fc3b895750ae28e86a26fe3646d1c9
Tobias Happ <tobias.happ@gmx.de>
2020-09-22 15:38:46 +0200

    Use jetbrains from unstable and liquidprompt from stable

--- modules/overlays/unstable.nix
+++ modules/overlays/unstable.nix
@@ -5,25 +5,12 @@ let
     config = { allowUnfree = true; };
     overlays = [ ];
   };
-
-  pinnedPkgs = import (super.fetchFromGitHub {
-    owner = "NixOS";
-    repo = "nixpkgs";
-    rev = "ee939c9d1878183e84054b1b0766b76ff33d2c20";
-    sha256 = "0364blqdvcahc58f577fhz7g15gz6gp8vmyxn6nzwpv73zq6hp7j";
-  }) {
-    config = { allowUnfree = true; };
-    overlays = [ ];
-  };
 in
 
 {
   inherit (unstable)
-    #jetbrains         # need bleeding edge version
-    liquidprompt      # need version 1.12.0
+    jetbrains         # need bleeding edge version
     teamspeak_server  # need bleeding edge version
     terraform         # need bleeding edge version
   ;
-
-  inherit (pinnedPkgs) jetbrains;
 }
