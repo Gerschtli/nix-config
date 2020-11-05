8d57b5db7036038a1d69f74d7e86955a09bd356d
Tobias Happ <tobias.happ@gmx.de>
2020-11-05 10:14:07 +0100

    Use gcloud from unstable

--- modules/overlays/unstable.nix
+++ modules/overlays/unstable.nix
@@ -9,6 +9,7 @@ in
 
 {
   inherit (unstable)
+    google-cloud-sdk  # need bleeding edge version
     jetbrains         # need bleeding edge version
     teamspeak_server  # need bleeding edge version
     terraform         # need bleeding edge version
