322170251439b6dc54aa887a0a4fbff227eca8a3
Tobias Happ <tobias.happ@gmx.de>
2020-11-08 15:18:31 +0100

    Add path constants in lib

--- modules/lib/default.nix
+++ modules/lib/default.nix
@@ -10,4 +10,14 @@ in
   inherit (fileList) getFileList getRecursiveFileList;
 
   containerApp = callPackage ./container-app.nix { };
+
+  path = {
+    modules = ../.;
+    config = ../config;
+    files = ../files;
+    hardwareConfiguration = ../hardware-configuration;
+    homeFiles = ../../home-manager-configurations/home-files;
+    overlays = ../overlays;
+    secrets = toString ../secrets;
+  };
 }
