699fab04318466a7d88439ed5e11bbdb35e26799
Tobias Happ <tobias.happ@gmx.de>
2020-11-08 22:29:42 +0100

    Remove default values

--- modules/config/wm/sway.nix
+++ modules/config/wm/sway.nix
@@ -115,43 +115,8 @@ in
         bars = [
           {
             inherit fonts;
-            mode = "dock";
-            hiddenState = "hide";
             position = "top";
-            workspaceButtons = true;
-            workspaceNumbers = true;
             statusCommand = "${pkgs.i3status}/bin/i3status";
-            trayOutput = "primary";
-            colors = {
-              background = "#000000";
-              statusline = "#ffffff";
-              separator = "#666666";
-              focusedWorkspace = {
-                border = "#4c7899";
-                background = "#285577";
-                text = "#ffffff";
-              };
-              activeWorkspace = {
-                border = "#333333";
-                background = "#5f676a";
-                text = "#ffffff";
-              };
-              inactiveWorkspace = {
-                border = "#333333";
-                background = "#222222";
-                text = "#888888";
-              };
-              urgentWorkspace = {
-                border = "#2f343a";
-                background = "#900000";
-                text = "#ffffff";
-              };
-              bindingMode = {
-                border = "#2f343a";
-                background = "#900000";
-                text = "#ffffff";
-              };
-            };
           }
         ];
       };
