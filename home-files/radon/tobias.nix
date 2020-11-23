670824968c641899150cc699c31328d5d1123a16
Tobias Happ <tobias.happ@gmx.de>
2020-11-23 12:00:18 +0100

    Add jdk8 to sdks for radon

--- home-files/radon/tobias.nix
+++ home-files/radon/tobias.nix
@@ -31,7 +31,7 @@ in
       sdks = {
         enable = true;
         links = {
-          inherit (pkgs) jdk11 python36 python37 python38;
+          inherit (pkgs) jdk8 jdk11 python36 python37 python38;
         };
       };
 
