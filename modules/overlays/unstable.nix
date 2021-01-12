fddb59c89dbcbbce472f68a05c552513f9705990
Tobias Happ <tobias.happ@gmx.de>
2021-01-12 20:16:29 +0100

    Use audacity from unstable channel

--- modules/overlays/unstable.nix
+++ modules/overlays/unstable.nix
@@ -9,6 +9,7 @@ in
 
 {
   inherit (unstable)
+    audacity          # FIXME: remove if https://github.com/NixOS/nixpkgs/commit/de1d520b58f2d9d771e54492dfda8b8981c9500d is in stable
     google-cloud-sdk  # need bleeding edge version
     jetbrains         # need bleeding edge version
     terraform         # need bleeding edge version
