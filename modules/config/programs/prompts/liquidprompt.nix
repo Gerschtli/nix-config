a991b3292c06734f266271555d8095c797421032
Tobias Happ <tobias.happ@gmx.de>
2021-06-07 21:58:43 +0200

    Update liquidprompt config

--- modules/config/programs/prompts/liquidprompt.nix
+++ modules/config/programs/prompts/liquidprompt.nix
@@ -6,40 +6,22 @@ let
   cfg = config.custom.programs.prompts.liquidprompt;
 
   liquidpromptConfig = {
-    LP_BATTERY_THRESHOLD = 75;
-    LP_LOAD_THRESHOLD = 60;
-    LP_TEMP_THRESHOLD = 60;
-    LP_ENABLE_SHORTEN_PATH = 1;
-    LP_PATH_LENGTH = 50;
-    LP_PATH_KEEP = 2;
-    LP_HOSTNAME_ALWAYS = 1;
-    LP_ENABLE_FQDN = 0;
-    LP_USER_ALWAYS = 1;
-    LP_PERCENTS_ALWAYS = 1;
-    LP_ENABLE_PERM = 1;
-    LP_ENABLE_PROXY = 0;
-    LP_ENABLE_JOBS = 0;
-    LP_ENABLE_LOAD = 1;
     LP_ENABLE_BATT = 0;
-    LP_ENABLE_SUDO = 0;
-    LP_ENABLE_VCS_ROOT = 1;
-    LP_ENABLE_GIT = 1;
-    LP_ENABLE_SVN = 0;
-    LP_ENABLE_HG = 0;
-    LP_ENABLE_FOSSIL = 0;
     LP_ENABLE_BZR = 0;
-    LP_ENABLE_TIME = 0;
-    LP_ENABLE_RUNTIME = 0;
-    LP_RUNTIME_THRESHOLD = 10;
-    LP_ENABLE_VIRTUALENV = 0;
+    LP_ENABLE_DETACHED_SESSIONS = 0;
+    LP_ENABLE_FOSSIL = 0;
+    LP_ENABLE_FQDN = 0;
+    LP_ENABLE_GIT = 1;
+    LP_ENABLE_HG = 0;
+    LP_ENABLE_JOBS = 0;
     LP_ENABLE_SCLS = 0;
+    LP_ENABLE_SVN = 0;
     LP_ENABLE_TEMP = 0;
-    LP_TIME_ANALOG = 0;
-    LP_ENABLE_TITLE = 0;
-    LP_ENABLE_SCREEN_TITLE = 0;
-    LP_ENABLE_SSH_COLORS = 0;
-    LP_DISABLED_VCS_PATH = "";
-  } // cfg.config;
+    LP_ENABLE_VCS_ROOT = 1;
+    LP_ENABLE_VIRTUALENV = 0;
+    LP_HOSTNAME_ALWAYS = 1;
+    LP_PATH_LENGTH = 50;
+  };
 in
 
 {
@@ -51,12 +33,6 @@ in
     custom.programs.prompts.liquidprompt = {
 
       enable = mkEnableOption "liquidprompt config";
-
-      config = mkOption {
-        type = types.attrsOf (types.either types.int types.str);
-        default = {};
-        description = "Liquidprompt config.";
-      };
 
     };
 
