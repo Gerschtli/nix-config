c6d939ad70cd2554b40cd5f78bd99ce33e7911f8
Tobias Happ <tobias.happ@gmx.de>
2021-06-12 14:02:09 +0200

    Clean up unstable overlay

--- modules/overlays/unstable.nix
+++ modules/overlays/unstable.nix
@@ -12,13 +12,9 @@ in
     audacity          # FIXME: remove if https://github.com/NixOS/nixpkgs/commit/de1d520b58f2d9d771e54492dfda8b8981c9500d is in stable
 
     # need bleeding edge version
-    google-cloud-sdk
     jetbrains
     portfolio
     teamspeak_server
-    terraform
-
-    jdk16
   ;
 
   nnn = unstable.nnn.override { withNerdIcons = true; };
