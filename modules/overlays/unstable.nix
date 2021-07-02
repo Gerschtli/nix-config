afd1eef143f3be51fa59951fde06cdcab18088a5
Tobias Happ <tobias.happ@gmx.de>
2021-07-02 22:53:20 +0200

    Remove audacity from unstable overlay

--- modules/overlays/unstable.nix
+++ modules/overlays/unstable.nix
@@ -9,8 +9,6 @@ in
 
 {
   inherit (unstable)
-    audacity          # FIXME: remove if https://github.com/NixOS/nixpkgs/commit/de1d520b58f2d9d771e54492dfda8b8981c9500d is in stable
-
     # need bleeding edge version
     jetbrains
     portfolio
