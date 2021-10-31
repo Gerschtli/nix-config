b45cc012f3c21fe22423a70cc843a303bba11de6
Tobias Happ <tobias.happ@gmx.de>
2021-10-31 19:35:30 +0100

    Refactor nvidia-offload script

--- modules/config/system/nvidia-optimus/default.nix
+++ modules/config/system/nvidia-optimus/default.nix
@@ -49,13 +49,12 @@ in
     ];
 
     environment.systemPackages = [
-      (pkgs.writeShellScriptBin "nvidia-offload" ''
-        export __NV_PRIME_RENDER_OFFLOAD=1
-        export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
-        export __GLX_VENDOR_LIBRARY_NAME=nvidia
-        export __VK_LAYER_NV_optimus=NVIDIA_only
-        exec -a "$0" "$@"
-      '')
+      (config.lib.custom.mkScript
+        "nvidia-offload"
+        ./nvidia-offload.sh
+        [ ]
+        { _doNotClearPath = true; }
+      )
     ];
 
     hardware.nvidia.prime = {
