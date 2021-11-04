7e8e25a034bff2388497cb88985354f83bfe3a16
Tobias Happ <tobias.happ@gmx.de>
2021-11-04 14:28:36 +0100

    Use pure-prompt from nixpkgs

--- modules/config/programs/prompts/pure.nix
+++ modules/config/programs/prompts/pure.nix
@@ -4,25 +4,6 @@ with lib;
 
 let
   cfg = config.custom.programs.prompts.pure;
-
-  # TODO: add in nixpkgs
-  pure = pkgs.stdenv.mkDerivation {
-    name = "pure";
-
-    src = pkgs.fetchFromGitHub {
-      owner = "sindresorhus";
-      repo = "pure";
-      rev = "v1.10.3";
-      sha256 = "0zjgnlw01ri0brx108n6miw4y0cxd6al1bh28m8v8ygshm94p1zx";
-    };
-
-    installPhase = ''
-      mkdir -p $out/share/pure
-
-      install -m 0644 pure.zsh $out/share/pure/prompt_pure_setup
-      install -m 0644 async.zsh $out/share/pure/async
-    '';
-  };
 in
 
 {
@@ -40,6 +21,8 @@ in
 
   config = mkIf cfg.enable {
 
+    home.sessionVariables.PURE_GIT_PULL = 0;
+
     programs.zsh = {
       initExtra = ''
         autoload -U promptinit; promptinit
@@ -49,7 +32,7 @@ in
       plugins = [
         {
           name = "pure";
-          src = "${pure}/share/pure";
+          src = "${pkgs.pure-prompt}/share/zsh/site-functions";
         }
       ];
     };
