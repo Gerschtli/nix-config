67dbf007a2171e01903488d31ae6da1e5f123e2c
Tobias Happ <tobias.happ@gmx.de>
2021-11-07 21:20:18 +0100

    Enable nix-direnv

--- modules/config/development/lorri/default.nix
+++ modules/config/development/lorri/default.nix
@@ -41,14 +41,11 @@ in
     programs = {
       direnv = {
         enable = true;
-        stdlib = ''
-          use_flake() {
-            watch_file flake.nix
-            watch_file flake.lock
-            mkdir --parents "$(direnv_layout_dir)"
-            eval "$(nix print-dev-env --profile "$(direnv_layout_dir)/flake-profile")"
-          }
-        '';
+
+        nix-direnv = {
+          enable = true;
+          enableFlakes = true;
+        };
       };
 
       zsh.initExtraBeforeCompInit = ''
