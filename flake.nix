86975683596e711ca9682a603dd76dc047470d91
Tobias Happ <tobias.happ@gmx.de>
2021-11-27 16:09:04 +0100

    flake: reduce different versions of same flakes

--- flake.nix
+++ flake.nix
@@ -4,28 +4,58 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05";
     unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    home-manager.url = "github:nix-community/home-manager/release-21.05";
-    nix-on-droid.url = "github:t184256/nix-on-droid";
-
-    agenix.url = "github:ryantm/agenix";
-    agenix-cli.url = "github:cole-h/agenix-cli";
-    # homeage.url = "github:jordanisaacs/homeage";
-    homeage.url = "github:Gerschtli/homeage/support-script";
-
-    dmenu.url = "github:Gerschtli/dmenu";
-    dwm.url = "github:Gerschtli/dwm";
-    dwm-status.url = "github:Gerschtli/dwm-status";
-    teamspeak-update-notifier.url = "github:Gerschtli/teamspeak-update-notifier";
 
     # commit before jdk15 drop
     # https://github.com/NixOS/nixpkgs/commit/9dde9d8b9ee4b7a4dfbb0ab1204d9f6f4a188360
     nixpkgs-for-jdk15.url = "github:NixOS/nixpkgs/df175b7f61d852dc599fe248b1a8666c312457bd";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-21.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nix-on-droid = {
+      url = "github:t184256/nix-on-droid";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
+    };
+
+    agenix = {
+      url = "github:ryantm/agenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    agenix-cli = {
+      url = "github:cole-h/agenix-cli";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    homeage = {
+      # url = "github:jordanisaacs/homeage";
+      url = "github:Gerschtli/homeage/support-script";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    dmenu = {
+      url = "github:Gerschtli/dmenu";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    dwm = {
+      url = "github:Gerschtli/dwm";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    dwm-status = {
+      url = "github:Gerschtli/dwm-status";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    teamspeak-update-notifier = {
+      url = "github:Gerschtli/teamspeak-update-notifier";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
     { self
     , nixpkgs
     , unstable
+    , nixpkgs-for-jdk15
     , home-manager
     , nix-on-droid
     , agenix
@@ -35,7 +65,6 @@
     , dwm
     , dwm-status
     , teamspeak-update-notifier
-    , nixpkgs-for-jdk15
     }: {
       overlay = final: prev: {
         inherit (nixpkgs-for-jdk15) jdk15;
