75d4a75d823b2b2e5f89762529d57b33b1a3a784
Tobias Happ <tobias.happ@gmx.de>
2021-11-27 17:03:33 +0100

    nixos.general: remove hercules-ci binary cache

--- nixos/base/general.nix
+++ nixos/base/general.nix
@@ -61,13 +61,11 @@ in
       binaryCaches = [
         "https://cache.nixos.org"
         "https://gerschtli.cachix.org"
-        "https://hercules-ci.cachix.org"
         "https://nix-on-droid.cachix.org"
       ];
       binaryCachePublicKeys = mkForce [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "gerschtli.cachix.org-1:dWJ/WiIA3W2tTornS/2agax+OI0yQF8ZA2SFjU56vZ0="
-        "hercules-ci.cachix.org-1:ZZeDl9Va+xe9j+KqdzoBZMFJHVQ42Uu/c/1/KMC5Lw0="
         "nix-on-droid.cachix.org-1:56snoMJTXmDRC1Ei24CmKoUqvHJ9XCp+nidK7qkMQrU="
       ];
       trustedUsers = [ "root" "tobias" ];
