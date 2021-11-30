3800cd6791dcac101b6dea3926ce829dfdf7a5ba
Tobias Happ <tobias.happ@gmx.de>
2021-11-30 20:09:43 +0100

    nixos.setup: update setup.sh for new structure

--- nixos/applications/tobias-happ/setup.sh
+++ nixos/applications/tobias-happ/setup.sh
@@ -1,15 +1,12 @@
 #! /usr/bin/env nix-shell
-#! nix-shell -i bash -p age coreutils curl findutils git gnugrep gnused hostname jq openssh
+#! nix-shell -i bash -p coreutils curl git gnugrep hostname jq openssh
 
 { # prevent execution if this script was only partially downloaded
 set -euo pipefail
 
 @bashLibContent@
 
-nixos="/etc/nixos"
-nixos_hm="${nixos}/home-manager-configurations"
-dotfiles="${HOME}/.dotfiles"
-dotfiles_hm="${dotfiles}/home-manager/home-manager-configurations"
+nix_config="${HOME}/.nix-config"
 
 _log() {
     echo
@@ -30,38 +27,6 @@ _clone() {
     git clone "${url}" "${directory}"
 }
 
-_exit_if_new_device() {
-    if [[ -n "${NEW_DEVICE}" ]]; then
-        _log "Please set up your nixos/home-manager configurations and rerun this script in cleanup mode!"
-        echo
-        exit
-    fi
-}
-
-_last_steps() {
-    if nix-env -q --json | jq ".[].pname" | grep '"nix"' > /dev/null; then
-        _log "Uninstall manual installed nix package..."
-        nix-env --uninstall nix
-    fi
-
-    if [[ -d "${dotfiles}" ]]; then
-        _log "Setup dotfiles..."
-        "${dotfiles}/bootstrap.sh"
-    fi
-
-    echo
-}
-
-
-NEW_DEVICE=
-mode=$(_read_enum "If you do not want to run the whole script, specify the mode" new-device cleanup "")
-if [[ "${mode}" = "new-device" ]]; then
-    NEW_DEVICE=1
-elif [[ "${mode}" = "cleanup" ]]; then
-    _last_steps
-    exit
-fi
-
 
 # generate ssh key and show
 echo
@@ -77,25 +42,23 @@ echo
 read -sr -n 1 -p "$(echo -e "${PURPLE}Press any key to continue...${RESET}")"
 echo
 
-# clone repos
-if _is_nixos && _is_root; then
-    if _read_boolean "Create backup of ${nixos} and clone configurations?"; then
-        mv -v "${nixos}"{,.bak}
-        _clone "nixos" git@github.com:Gerschtli/nixos-configurations.git "${nixos}"
-    fi
 
-    _clone "home-manager-configurations" git@github.com:Gerschtli/home-manager-configurations.git "${nixos_hm}"
+# clone repos
+if ! _is_nixos || _is_root; then
+    _clone "nix-config" git@github.com:Gerschtli/nix-config.git "${nix_config}"
 fi
 
-if ! _is_root && ( ! _is_nixos || _read_boolean "Install dotfiles?" ); then
-    _clone "dotfiles" git@github.com:Gerschtli/dotfiles.git "${dotfiles}"
+if ! _is_root; then
+    if _read_boolean "Install atom-config repo?"; then
+        if [[ -e "${HOME}/.atom" ]]; then
+            mv -v "${HOME}/.atom" "${HOME}/.atom.bak"
+        fi
 
-    if ! _is_nixos; then
-        _clone "home-manager-configurations" git@github.com:Gerschtli/home-manager-configurations.git "${dotfiles_hm}"
+        _clone "atom-config" git@github.com:Gerschtli/atom-config.git "${HOME}/.atom"
     fi
 
-    if _read_boolean "Install gpg repo?"; then
-        _clone "gpg repo" gitea@git.tobias-happ.de:Gerschtli/gpg.git "${dotfiles}/gpg"
+    if _read_boolean "Install gnupg-setup repo?"; then
+        _clone "gnupg repo" gitea@git.tobias-happ.de:Gerschtli/gnupg-setup.git "${HOME}/.gnupg-setup"
 
         if _read_boolean "Install password-store?"; then
             _clone "password store" gitea@git.tobias-happ.de:Gerschtli/pass.git "${HOME}/.password-store"
@@ -108,71 +71,48 @@ _clone "age-bak" gitea@git.tobias-happ.de:Gerschtli/age-bak.git "${HOME}/.age-ba
 _log "Change permissions of ~/.age-bak..."
 chmod -v 0700 "${HOME}/.age-bak"
 
-if _read_boolean "Create ~/.age/key.txt?"; then
-    _log "age" "generate age key"
-    mkdir -p "${HOME}/.age"
-    age-keygen -o "${HOME}/.age/key.txt"
+_log "Link ~/.age to ~/.age-bak..."
+ln -snv "${HOME}/.age" "${HOME}/.age-bak"
 
+
+# preparation for non nixos systems
+if nix-env -q --json | jq ".[].pname" | grep '"nix"' > /dev/null; then
+    _log "Set priority of installed nix package..."
+    nix-env --set-flag priority 1000 nix
 fi
 
 
+# installation
 if _is_nixos && _is_root; then
-    _log "Setup nix-channels..."
-    parameter=$(_read_enum "Enter setup-nix-channels arg" --nixos --small)
-    "${nixos_hm}/bin/setup-nix-channels" "${parameter}"
+    host_name=$(_read_enum "Enter hostname" krypton neon xenon)
+
+    _log "Run nixos-rebuild switch..."
+    nixos-rebuild switch --keep-going --flake "${nix_config}#${host_name}"
+elif [[ "${USER}" == "nix-on-droid" ]]; then
+    _log "Build nix-on-droid activationPackage..."
+    nix build "${nix_config}#nixOnDroidConfigurations.oneplus5.activationPackage" --impure
+
+    _log "Run activate script..."
+    ./result/activate
+
+    rm -v result
 elif ! _is_nixos && ! _is_root; then
-    _log "Setup nix-channels..."
-    parameter=$(_read_enum "Enter setup-nix-channels arg" --android --non-nixos)
-    "${dotfiles_hm}/bin/setup-nix-channels" "${parameter}"
+    _log "Build home-manager activationPackage..."
+    nix build "${nix_config}#homeConfigurations.${USER}@$(hostname).activationPackage"
+
+    _log "Run activate script..."
+    HOME_MANAGER_BACKUP_EXT=hm-bak ./result/activate
+
+    rm -v result
 fi
 
 
-if [[ -f "${HOME}/.nix-channels" ]]; then
-    _log "Update nix-channels..."
-    nix-channel --update
+# clean up
+if nix-env -q --json | jq ".[].pname" | grep '"nix"' > /dev/null; then
+    _log "Uninstall manual installed nix package..."
+    nix-env --uninstall nix
 fi
 
-
-if _is_nixos && _is_root; then
-    _exit_if_new_device
-
-    parameter=$(_read_enum "Enter hostname" argon krypton neon xenon)
-    ln -vsnf "${nixos}/configuration-${parameter}.nix" "${nixos}/configuration.nix"
-    nixos-rebuild switch || _read_boolean "Was nixos-rebuild successful?"
-elif ! _is_nixos && ! _is_root; then
-    home_file="${dotfiles_hm}/home-files/$(hostname)/$(whoami).nix"
-
-    if [[ "${USER}" == "nix-on-droid" ]]; then
-        _log "Link home.nix file..."
-        ln -vsnf "${home_file}" "${HOME}/.config/nixpkgs/home.nix"
-
-        _log "Link nix-on-droid.nix file..."
-        ln -vsnf "${dotfiles}/nix-on-droid/nix-on-droid.nix" "${HOME}/.config/nixpkgs/nix-on-droid.nix"
-
-        _log "Run nix-on-droid switch..."
-        nix-on-droid switch --verbose
-    else
-        if [[ -z "${NIX_PATH}" ]]; then
-            export NIX_PATH="${HOME}/.nix-defexpr/channels"
-        fi
-
-        if ! _available home-manager; then
-            _log "Install home-manager..."
-            nix-shell '<home-manager>' -A install
-        fi
-
-        if nix-env -q --json | jq ".[].pname" | grep '"nix"' > /dev/null; then
-            _log "Set priority of installed nix package..."
-            nix-env --set-flag priority 1000 nix
-        fi
-
-        _exit_if_new_device
-
-        _log "Run home-manager switch..."
-        home-manager switch -b hm-bak -f "${home_file}"
-    fi
-fi
-
-_last_steps
+echo
 
 }
