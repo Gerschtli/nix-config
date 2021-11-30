4a731925ba5253df1af0eb8ddc23f7bbc0619f7b
Tobias Happ <tobias.happ@gmx.de>
2021-11-30 20:30:10 +0100

    home.zsh: remove outdated directory hashes

--- home/programs/zsh/directory-hash.zsh
+++ home/programs/zsh/directory-hash.zsh
@@ -1,32 +1,7 @@
 unhash -dm "*"
 
 _set-hashes() {
-    local directories
-    typeset -A directories
-
-    local dotfiles="${HOME}/.dotfiles"
-    local home_manager="${dotfiles}/home-manager/home-manager-configurations"
-
-    directories=(
-        "dotfiles" "${dotfiles}"
-        "gpg" "${dotfiles}/gpg"
-        "home-manager" "${home_manager}"
-    )
-
-    if [[ $(id -u) == 0 ]]; then
-        local nixos="/etc/nixos"
-        local home_manager_nixos="${nixos}/home-manager-configurations"
-
-        directories+=(
-           "nixos" "${nixos}"
-           "home-manager" "${home_manager_nixos}"
-        )
-    fi
-
-    local name
-    for name in ${(k)directories}; do
-        [[ -d "${directories[$name]}" ]] && hash -d "${name}"="${directories[$name]}"
-    done
+    hash -d "nix-config"="${HOME}/.nix-config"
 
     local projects="${HOME}/projects"
 
