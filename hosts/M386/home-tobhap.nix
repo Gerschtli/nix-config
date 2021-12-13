302cd1367a6b795c5a7fc932d0b55474aa1d6935
Tobias Happ <tobias.happ@gmx.de>
2021-12-13 18:31:19 +0100

    hosts.M386: setup kubernetes

--- hosts/M386/home-tobhap.nix
+++ hosts/M386/home-tobhap.nix
@@ -32,9 +32,21 @@
     programs.ssh.modules = [ "sedo" ];
   };
 
-  home.sessionVariables = {
-    # see: https://github.com/NixOS/nixpkgs/issues/38991#issuecomment-400657551
-    LOCALE_ARCHIVE_2_11 = "/usr/bin/locale/locale-archive";
-    LOCALE_ARCHIVE_2_27 = "${pkgs.glibcLocales}/lib/locale/locale-archive";
+  home = {
+    packages = with pkgs; [
+      kubectl
+      kubernetes-helm
+    ];
+
+    sessionVariables = {
+      # see: https://github.com/NixOS/nixpkgs/issues/38991#issuecomment-400657551
+      LOCALE_ARCHIVE_2_11 = "/usr/bin/locale/locale-archive";
+      LOCALE_ARCHIVE_2_27 = "${pkgs.glibcLocales}/lib/locale/locale-archive";
+
+      KUBECONFIG = lib.concatMapStringsSep ":" (path: "${config.home.homeDirectory}/.kube/${path}") [
+        "config"
+        "integration/config"
+      ];
+    };
   };
 }
