49c0accb9fbd9c9c85bfb223535a670c43f0b61c
Tobias Happ <tobias.happ@gmx.de>
2022-06-06 15:12:55 +0200

    hosts.krypton: update hardware-configuration

--- hosts/krypton/hardware-configuration.nix
+++ hosts/krypton/hardware-configuration.nix
@@ -23,4 +23,12 @@
   swapDevices =
     [{ device = "/dev/disk/by-uuid/94c0be7f-2f66-449b-90c9-46ed847df3cc"; }];
 
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.eth0.useDHCP = lib.mkDefault true;
+
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
