9829970636c80403b1f4189ccd3225864e4d6559
Tobias Happ <tobias.happ@gmx.de>
2022-06-06 14:53:03 +0200

    hosts.neon: update hardware-configuration

--- hosts/neon/hardware-configuration.nix
+++ hosts/neon/hardware-configuration.nix
@@ -29,5 +29,14 @@
   swapDevices =
     [{ device = "/dev/disk/by-uuid/209efe2a-0bd4-41a6-9f0b-253e6f92a8f7"; }];
 
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.docker0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.eth0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlan0.useDHCP = lib.mkDefault true;
+
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
