c122862d13ba375c7c2864d81f25c75282da7fba
Tobias Happ <tobias.happ@gmx.de>
2022-12-15 19:12:16 +0100

    home.direnv: store direnv cache outside of projects

--- home/development/direnv/default.nix
+++ home/development/direnv/default.nix
@@ -58,6 +58,18 @@ in
         enable = true;
 
         nix-direnv.enable = true;
+
+        stdlib = ''
+          # from https://github.com/direnv/direnv/wiki/Customizing-cache-location
+          declare -A direnv_layout_dirs
+          direnv_layout_dir() {
+            echo "''${direnv_layout_dirs[$PWD]:=$(
+              local hash="$(sha1sum - <<<"$PWD" | cut -c-7)"
+              local path="''${PWD//[^a-zA-Z0-9]/-}"
+              echo "''${XDG_CACHE_HOME:-"$HOME/.cache"}/direnv/layouts/''${hash}''${path}"
+            )}"
+          }
+        '';
       };
 
       zsh.initExtraBeforeCompInit = ''
