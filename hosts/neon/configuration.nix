cde8e2143261421e0a179db3a27d0048f2bba508
Tobias Happ <tobias.happ@gmx.de>
2023-02-07 15:58:18 +0100

    hosts.neon: update to impermanence+btrfs setup

--- hosts/neon/configuration.nix
+++ hosts/neon/configuration.nix
@@ -1,9 +1,13 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, inputs, ... }:
 
 {
+  imports = [ inputs.impermanence.nixosModules.impermanence ];
+
   boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
 
   custom = {
+    agenix.secrets = [ "passwd-root-neon" "passwd-tobias-neon" ];
+
     base.desktop = {
       enable = true;
       laptop = true;
@@ -22,5 +26,98 @@
     };
   };
 
-  systemd.suppressedSystemUnits = [ "systemd-backlight@backlight:acpi_video0.service" ];
+  # agenix needs to wait for impermanence
+  system.activationScripts.agenixNewGeneration.deps = [ "persist-files" ];
+
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.supportedFilesystems = [ "btrfs" ];
+  hardware.enableAllFirmware = true;
+  nixpkgs.config.allowUnfree = true;
+
+  environment.persistence."/persist" = {
+    hideMounts = true;
+    directories = [
+      "/etc/NetworkManager/system-connections"
+      "/root/.local/share/nix"
+      "/root/.nix-config"
+      "/var/lib/docker"
+      "/var/lib/fail2ban"
+      "/var/lib/systemd/coredump"
+    ];
+    files = [
+      "/etc/adjtime"
+      "/etc/machine-id"
+      "/root/.bash_history"
+      "/root/.config/zsh/.zsh_history"
+      { file = "/root/.age/key.txt"; parentDirectory = { mode = "0700"; }; }
+      { file = "/root/.ssh/known_hosts"; parentDirectory = { mode = "0700"; }; }
+    ];
+
+    users.tobias = {
+      directories = [
+        ".config/Code"
+        ".config/google-chrome"
+        ".files"
+        ".local/share/direnv"
+        ".local/share/nix"
+        ".local/state/pnpm"
+        ".password-store"
+        ".rustup"
+        ".thunderbird"
+        ".vscode"
+        "Documents"
+        "Downloads"
+        "projects"
+        { directory = ".gnupg"; mode = "0700"; }
+      ];
+      files = [
+        ".bash_history"
+        ".config/zsh/.zsh_history"
+        ".xsession-errors"
+        { file = ".age/key.txt"; parentDirectory = { mode = "0700"; }; }
+        { file = ".ssh/known_hosts"; parentDirectory = { mode = "0700"; }; }
+      ];
+    };
+  };
+
+  security.sudo.extraConfig = ''
+    Defaults lecture = never
+  '';
+
+  boot.initrd.postDeviceCommands = lib.mkBefore ''
+    mkdir -p /mnt
+
+    mount -o subvol=/ /dev/vg/root /mnt
+
+    # remove auto-created subvolumes in /root subvolume
+    # - /root/srv
+    # - /root/var/lib/portables # from systemd
+    # - /root/var/lib/machines # from systemd
+    btrfs subvolume list -o /mnt/root \
+      | cut -f9 -d' ' \
+      | while read subvolume; do
+        echo "deleting /$subvolume subvolume..."
+        btrfs subvolume delete "/mnt/$subvolume"
+      done
+
+    echo "creating /root snapshot..."
+    btrfs subvolume snapshot -r /mnt/root "/mnt/root-$(date +"%Y-%m-%d-%H-%M-%S")"
+
+    echo "deleting /root subvolume..."
+    btrfs subvolume delete /mnt/root
+
+    echo "restoring blank /root subvolume..."
+    btrfs subvolume snapshot /mnt/root-blank /mnt/root
+
+    umount /mnt
+  '';
+
+  users = {
+    mutableUsers = false;
+
+    users = {
+      root.passwordFile = config.age.secrets.passwd-root-neon.path;
+      tobias.passwordFile = config.age.secrets.passwd-tobias-neon.path;
+    };
+  };
 }
--- hosts/neon/hardware-configuration.nix
+++ hosts/neon/hardware-configuration.nix
@@ -9,15 +9,39 @@
       (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "usb_storage" "usbhid" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ "dm-snapshot" ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
     {
-      device = "/dev/disk/by-uuid/aadb49f0-6242-420d-a4ed-30a0f4f39580";
-      fsType = "ext4";
+      device = "/dev/disk/by-uuid/6cc6eddf-9d97-4151-95b1-24ed0bea3816";
+      fsType = "btrfs";
+      options = [ "subvol=root" "compress=zstd" "noatime" ];
+    };
+
+  fileSystems."/nix" =
+    {
+      device = "/dev/disk/by-uuid/6cc6eddf-9d97-4151-95b1-24ed0bea3816";
+      fsType = "btrfs";
+      options = [ "subvol=nix" "compress=zstd" "noatime" ];
+    };
+
+  fileSystems."/persist" =
+    {
+      device = "/dev/disk/by-uuid/6cc6eddf-9d97-4151-95b1-24ed0bea3816";
+      fsType = "btrfs";
+      options = [ "subvol=persist" "compress=zstd" "noatime" ];
+      neededForBoot = true;
+    };
+
+  fileSystems."/var/log" =
+    {
+      device = "/dev/disk/by-uuid/6cc6eddf-9d97-4151-95b1-24ed0bea3816";
+      fsType = "btrfs";
+      options = [ "subvol=log" "compress=zstd" "noatime" ];
+      neededForBoot = true;
     };
 
   fileSystems."/boot" =
@@ -26,8 +50,14 @@
       fsType = "vfat";
     };
 
+  fileSystems."/root/btrfs" =
+    { device = "/dev/disk/by-uuid/6cc6eddf-9d97-4151-95b1-24ed0bea3816";
+      fsType = "btrfs";
+      options = [ "subvol=/" "compress=zstd" "noatime" ];
+    };
+
   swapDevices =
-    [{ device = "/dev/disk/by-uuid/209efe2a-0bd4-41a6-9f0b-253e6f92a8f7"; }];
+    [{ device = "/dev/disk/by-uuid/db7ba2be-681c-4ee6-9da5-c2c3b9d87534"; }];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
@@ -38,5 +68,6 @@
   # networking.interfaces.eth0.useDHCP = lib.mkDefault true;
   # networking.interfaces.wlan0.useDHCP = lib.mkDefault true;
 
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
