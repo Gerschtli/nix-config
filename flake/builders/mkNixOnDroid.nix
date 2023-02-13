fcea15ca442dfcdc84cb91409eb3936c32ab33b8
Tobias Happ <tobias.happ@gmx.de>
2023-02-13 11:43:01 +0100

    flake.mkNixOnDroid: add lib.custom for nix-on-droid

--- flake/builders/mkNixOnDroid.nix
+++ flake/builders/mkNixOnDroid.nix
@@ -1,8 +1,29 @@
-{ inputs, rootPath, system, pkgsNixOnDroidFor, homeModulesFor, name, ... }:
+{ inputs, rootPath, system, pkgsNixOnDroidFor, customLibFor, homeModulesFor, name, ... }:
+
+let
+  inherit (pkgsNixOnDroidFor.${system}) lib;
+in
 
 inputs.nix-on-droid.lib.nixOnDroidConfiguration {
   pkgs = pkgsNixOnDroidFor.${system};
-  modules = [ "${rootPath}/hosts/${name}/nix-on-droid.nix" ];
+  modules = [
+    "${rootPath}/hosts/${name}/nix-on-droid.nix"
+
+    {
+      _file = ./mkNixOnDroid.nix;
+
+      options.lib = lib.mkOption {
+        type = lib.types.attrsOf lib.types.attrs;
+        default = { };
+        description = ''
+          This option allows modules to define helper functions,
+          constants, etc.
+        '';
+      };
+
+      config.lib.custom = customLibFor.${system};
+    }
+  ];
 
   extraSpecialArgs = {
     inherit inputs rootPath;
