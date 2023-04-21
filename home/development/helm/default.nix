2c328fbc0ee6ee45f2b174b63ad0566f22eff6d2
Tobias Happ <tobias.happ@gmx.de>
2023-04-21 12:23:52 +0200

    home.helm: add more scripts and aliases

--- home/development/helm/default.nix
+++ home/development/helm/default.nix
@@ -7,6 +7,12 @@ let
     ;
 
   cfg = config.custom.development.helm;
+
+  helm-upgrade = config.lib.custom.mkScript
+    "helm-upgrade"
+    ./helm-upgrade.sh
+    (with pkgs; [ kubernetes-helm ])
+    { };
 in
 
 {
@@ -26,21 +32,31 @@ in
 
     custom.programs.shell.shellAliases = {
       helm-int = "helm --kube-context integration --namespace integration";
+      helm-stage = "helm --kube-context stage --namespace stage";
+      helm-int-tobhap-cleanup = "helm-int uninstall $(helm-int list | grep '^tobhap-' | awk '{print $1}')";
     };
 
     home.packages = [
       pkgs.kubernetes-helm
 
-      (config.lib.custom.mkScript
-        "helm-upgrade"
-        ./helm-upgrade.sh
-        (with pkgs; [ kubernetes-helm ])
-        { }
-      )
+      helm-upgrade
 
       (config.lib.custom.mkZshCompletion
         "helm-upgrade"
         ./helm-upgrade-completion.zsh
+        { }
+      )
+
+      (config.lib.custom.mkScript
+        "helm-upgrade-this"
+        ./helm-upgrade-this.sh
+        (with pkgs; [ git gnused helm-upgrade kubernetes-helm ])
+        { }
+      )
+
+      (config.lib.custom.mkZshCompletion
+        "helm-upgrade-this"
+        ./helm-upgrade-this-completion.zsh
         { }
       )
     ];
---
+++ home/development/helm/helm-upgrade-this-completion.zsh
@@ -0,0 +1,3 @@
+_arguments \
+    "1:environment:(tobhap integration stage)" \
+    "2:options:(--deps-build)"
