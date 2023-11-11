6908e3f3e9e3101a444469685b48a4f18f1b5e44
Tobias Happ <tobias.happ@gmx.de>
2023-11-11 18:16:15 +0100

    files.tmux: remove pnpm dev for vereinsmanager profile

--- files/tmux/profiles/vereinsmanager.sh
+++ files/tmux/profiles/vereinsmanager.sh
@@ -3,6 +3,5 @@ source "${PATH_TO_CONF_DIR}/util/helpers.sh"
 ROOT="${HOME}/projects/vereinsmanager"
 
 CMD_PRIMARY="git fm"
-CMD_SECONDARY="pnpm run dev"
 
 SIDE_CMDS=( "$(_cmds "docker compose up -d db" "pnpm drizzle-kit studio")" )
