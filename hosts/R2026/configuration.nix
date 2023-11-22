f6d9d9f48ac4e883fcae8dd999f2747d6e5d0c56
Tobias Happ <tobias.happ@gmx.de>
2023-11-22 13:09:12 +0100

    hosts.R2026: set up yabai and skhd

--- hosts/R2026/configuration.nix
+++ hosts/R2026/configuration.nix
@@ -59,10 +59,106 @@ in
 
   security.pam.enableSudoTouchIdAuth = true;
 
+  services.yabai.enable = true;
+  services.yabai.config = {
+    layout = "bsp"; # binary spaced partition
+    focus_follows_mouse = "autoraise";
+    window_placement = "second_child";
+
+    # modifier for clicking and dragging with mouse
+    mouse_modifier = "alt";
+    # set modifier + left-click drag to move window
+    mouse_action1 = "move";
+    # set modifier + right-click drag to resize window
+    mouse_action2 = "resize";
+
+    # when window is dropped in center of another window, swap them (on edges it will split it)
+    mouse_drop_action = "swap";
+  };
+  services.yabai.extraConfig = ''
+    yabai -m rule --add app='Calculator' manage=off
+    yabai -m rule --add app='System Settings' manage=off
+  '';
+  services.skhd.enable = true;
+  services.skhd.skhdConfig = ''
+    # -- Changing Window Focus --
+
+    # change window focus within space
+    alt - j : yabai -m window --focus south
+    alt - k : yabai -m window --focus north
+    alt - h : yabai -m window --focus west
+    alt - l : yabai -m window --focus east
+
+    #change focus between external displays (left and right)
+    alt - s: yabai -m display --focus west
+    alt - g: yabai -m display --focus east
+
+    # -- Modifying the Layout --
+
+    # rotate layout clockwise
+    shift + alt - r : yabai -m space --rotate 270
+
+    # flip along y-axis
+    shift + alt - y : yabai -m space --mirror y-axis
+
+    # flip along x-axis
+    shift + alt - x : yabai -m space --mirror x-axis
+
+    # toggle window float
+    shift + alt - t : yabai -m window --toggle float --grid 4:4:1:1:2:2
+
+
+    # -- Modifying Window Size --
+
+    # maximize a window
+    shift + alt - m : yabai -m window --toggle zoom-fullscreen
+
+    # balance out tree of windows (resize to occupy same area)
+    shift + alt - e : yabai -m space --balance
+
+    # -- Moving Windows Around --
+
+    # swap windows
+    shift + alt - j : yabai -m window --swap south
+    shift + alt - k : yabai -m window --swap north
+    shift + alt - h : yabai -m window --swap west
+    shift + alt - l : yabai -m window --swap east
+
+    # move window and split
+    ctrl + alt - j : yabai -m window --warp south
+    ctrl + alt - k : yabai -m window --warp north
+    ctrl + alt - h : yabai -m window --warp west
+    ctrl + alt - l : yabai -m window --warp east
+
+    # move window to display left and right
+    shift + alt - s : yabai -m window --display west; yabai -m display --focus west;
+    shift + alt - g : yabai -m window --display east; yabai -m display --focus east;
+
+
+    # move window to prev and next space
+    shift + alt - p : yabai -m window --space prev;
+    shift + alt - n : yabai -m window --space next;
+
+    # move window to space #
+    shift + alt - 1 : yabai -m window --space 1;
+    shift + alt - 2 : yabai -m window --space 2;
+    shift + alt - 3 : yabai -m window --space 3;
+    shift + alt - 4 : yabai -m window --space 4;
+    shift + alt - 5 : yabai -m window --space 5;
+    shift + alt - 6 : yabai -m window --space 6;
+    shift + alt - 7 : yabai -m window --space 7;
+
+    # -- Starting/Stopping/Restarting Yabai --
+
+    # stop/start/restart yabai
+    ctrl + alt - q : launchctl stop org.nixos.yabai
+    ctrl + alt - s : launchctl start org.nixos.yabai
+    ctrl + alt - r : launchctl stop org.nixos.yabai; launchctl start org.nixos.yabai
+  '';
+
   # TODO
   # cachix-agent
   # nix-darwin CI
-  # yabai
   # lorri
   # Hidden bar
   # Alttab
@@ -84,5 +180,8 @@ in
   # install chrome, intellij, iterm2, docker, vscode
   # iterm2 set UbuntuMono font, size 13, h 85, v 115, solarized dark, window size
   # disable keyboard shortcuts for "Input Sources" in system settings
+  # enable keyboard shortcuts for Mission Control to switch to desktops
+  # (A11y > Desktop > Enable Reduce Motion)
+  # Desktop & Dock > Mission Control > Disable "Automatically rearrange Spaces based on most recent use
 
 }
