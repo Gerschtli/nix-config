9bd080b4b6ecef6128cd056de8f0798b631aa355
Tobias Happ <tobias.happ@gmx.de>
2024-01-04 17:04:58 +0100

    home.direnv: specify absolute path for sha1sum

--- home/development/direnv/default.nix
+++ home/development/direnv/default.nix
@@ -52,7 +52,7 @@ in
           declare -A direnv_layout_dirs
           direnv_layout_dir() {
             echo "''${direnv_layout_dirs[$PWD]:=$(
-              local hash="$(sha1sum - <<<"$PWD" | cut -c-7)"
+              local hash="$(${pkgs.coreutils}/bin/sha1sum - <<<"$PWD" | cut -c-7)"
               local path="''${PWD//[^a-zA-Z0-9]/-}"
               echo "''${XDG_CACHE_HOME:-"$HOME/.cache"}/direnv/layouts/''${hash}''${path}"
             )}"
