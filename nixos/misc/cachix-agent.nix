735f54ca7e9d8ade096460c1837d7f2fb6e43d7b
Tobias Happ <tobias.happ@gmx.de>
2024-03-13 12:22:39 +0100

    nixos.cachix-agent: only set to KillMode control-group for desktops
    Deployments fail when cachix-agent is restarted during activation
    because it kills the deployment itself when set to control-group.

--- nixos/misc/cachix-agent.nix
+++ nixos/misc/cachix-agent.nix
@@ -34,9 +34,11 @@ in
       credentialsFile = config.age.secrets."cachix-agent-token-${hostName}".path;
     };
 
-    # upstream sets this to process which means that subprocesses are not killed and stopping the service does not stop
-    # the nix deployment process. control-group is the default value.
-    systemd.services.cachix-agent.serviceConfig.KillMode = mkForce "control-group";
+    # Upstream sets this to process which means that subprocesses are not killed and stopping the service does not stop
+    # the nix deployment process. Control-group is the default value.
+    # Only set to control-group for desktop systems to prevent that system halt might be prevented by orphaned nix
+    # processes.
+    systemd.services.cachix-agent.serviceConfig.KillMode = mkIf config.custom.base.desktop.enable (mkForce "control-group");
 
   };
 
