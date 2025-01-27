91842a1fbacf6d593affbe40de79f0d7f9d783bf
Tobias Happ <tobias.happ@gmx.de>
2025-01-27 16:53:03 +0100

    home.server: enable hm gc when host is server

---
+++ home/base/server.nix
@@ -0,0 +1,29 @@
+{ config, lib, pkgs, nixosConfig, ... }:
+
+let
+  inherit (lib)
+    mkIf
+    ;
+in
+
+{
+
+  ###### interface
+
+  options = { };
+
+
+  ###### implementation
+
+  config = mkIf (nixosConfig != null && nixosConfig.custom.base.server.enable) {
+
+    # needed because of https://github.com/NixOS/nix/issues/8508
+    nix.gc = mkIf (config.home.username != "root") {
+      automatic = true;
+      frequency = "*-*-* 00:30:00";
+      options = "--delete-older-than 14d";
+    };
+
+  };
+
+}
