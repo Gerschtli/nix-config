eff85cbc7a1f7dc716f65ae1936b5280707989c8
Tobias Happ <tobias.happ@gmx.de>
2025-07-13 15:27:06 +0200

    chore: remove vereinsmanager tprofile

--- files/tmux/profiles/vereinsmanager.sh
+++
@@ -1,7 +0,0 @@
-source "${PATH_TO_CONF_DIR}/util/helpers.sh"
-
-ROOT="${HOME}/projects/vereinsmanager"
-
-CMD_PRIMARY="git fm"
-
-SIDE_CMDS=( "$(_cmds "docker compose up -d db" "pnpm drizzle-kit studio")" )
